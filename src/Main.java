cff84043455395e26da66384504107c654ef0867
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-09 14:17:52 +0800

    场景的切换

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	Button button;
+	Stage window;
+	Scene scene1, scene2;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -13,17 +16,29 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Title of the window");
+		window = primaryStage;
 
-		button = new Button();
-		button.setText("Click me");
+		Label label1 = new Label("Welcome to the first scene");
+		Button button1 = new Button("Go to the scene 2");
+		button1.setOnAction(x -> {
+			window.setScene(scene2);
+		});
+		VBox layout1 = new VBox(20);
+		layout1.getChildren().addAll(label1, button1);
+		scene1 = new Scene(layout1, 200, 200);
 
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
+		Label label2 = new Label("Welcome to the second scene!!!!!!!!");
+		Button button2 = new Button("Go to the scene 1");
+		button2.setOnAction(x -> {
+			window.setScene(scene1);
+		});
+		StackPane layout2 = new StackPane();
+		layout2.getChildren().addAll(label2, button2);
+		scene2 = new Scene(layout2, 600, 300);
 
-		Scene scene = new Scene(layout, 300, 250);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		window.setTitle("Title of the window");
+		window.setScene(scene1);
+		window.show();
 
 	}
 }
