0af22ae54bef984c29b361d090d36549b33df6aa
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-09 11:40:15 +0800

    匿名对象对按钮事件响应

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -17,6 +19,13 @@ public class Main extends Application {
 
 		button = new Button();
 		button.setText("Click me");
+
+		button.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				System.out.println("clicked checkout");
+			}
+		});
 
 		StackPane layout = new StackPane();
 		layout.getChildren().add(button);
