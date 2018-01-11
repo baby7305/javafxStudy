72b0bbf765d1fca36e8e79cab896f00c748f7523
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-11 17:30:08 +0800

    图层布局之BorderPane

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	Button button;
-
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -15,13 +15,23 @@ public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("Title of the window");
 
-		button = new Button();
-		button.setText("Click me");
+		HBox topMenu = new HBox();
+		Button buttonA = new Button("File");
+		Button buttonB = new Button("Edit");
+		Button buttonC = new Button("View");
+		topMenu.getChildren().addAll(buttonA, buttonB, buttonC);
 
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
+		VBox leftMenu = new VBox();
+		Button buttonD = new Button("D");
+		Button buttonE = new Button("E");
+		Button buttonF = new Button("F");
+		leftMenu.getChildren().addAll(buttonD, buttonE, buttonF);
 
-		Scene scene = new Scene(layout, 300, 250);
+		BorderPane borderPane = new BorderPane();
+		borderPane.setTop(topMenu);
+		borderPane.setLeft(leftMenu);
+
+		Scene scene = new Scene(borderPane, 300, 250);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 
