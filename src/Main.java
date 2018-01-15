2228472bede6d1e2ce3f0f2b944dd56e1c77eb82
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-15 10:57:59 +0800

    checkbox测试

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	Stage window;
+	Scene scene;
 	Button button;
 
 	public static void main(String[] args) {
@@ -13,17 +17,38 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Title of the window");
+		window = primaryStage;
+		window.setTitle("Bucky's Meat Subs");
 
-		button = new Button();
-		button.setText("Click me");
+		//Checkboxes
+		CheckBox box1 = new CheckBox("Bacon");
+		CheckBox box2 = new CheckBox("Tuna");
+		box2.setSelected(true);
 
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
+		//Button
+		button = new Button("Order Now!");
+		button.setOnAction(e -> handleOptions(box1, box2));
 
-		Scene scene = new Scene(layout, 300, 250);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		//Layout
+		VBox layout = new VBox(10);
+		layout.setPadding(new Insets(20, 20, 20, 20));
+		layout.getChildren().addAll(box1, box2, button);
 
+		scene = new Scene(layout, 300, 250);
+		window.setScene(scene);
+		window.show();
+	}
+
+	//Handle checkbox options
+	private void handleOptions(CheckBox box1, CheckBox box2){
+		String message = "Users order:\n";
+
+		if(box1.isSelected())
+			message += "Bacon\n";
+
+		if(box2.isSelected())
+			message += "Tuna\n";
+
+		System.out.println(message);
 	}
 }
