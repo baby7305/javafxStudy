5d1bc2bc356b0218e3309ac2b60394e38fdf583a
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-16 18:14:29 +0800

    listView测试

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	Stage window;
+	Scene scene;
 	Button button;
+	ListView<String> listView;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -13,17 +20,33 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Title of the window");
+		window = primaryStage;
+		window.setTitle("ListView Demo");
+		button = new Button("Submit");
 
-		button = new Button();
-		button.setText("Click me");
+		listView = new ListView<>();
+		listView.getItems().addAll("Iron Man", "Titanic", "Contact", "Surrogates");
+		listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
+		button.setOnAction(e -> buttonClicked());
 
-		Scene scene = new Scene(layout, 300, 250);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		VBox layout = new VBox(10);
+		layout.setPadding(new Insets(20, 20, 20, 20));
+		layout.getChildren().addAll(listView, button);
 
+		scene = new Scene(layout, 300, 250);
+		window.setScene(scene);
+		window.show();
+	}
+
+	private void buttonClicked(){
+		String message = "";
+		ObservableList<String> movies;
+		movies = listView.getSelectionModel().getSelectedItems();
+
+		for(String m: movies)
+			message += m + "\n";
+
+		System.out.println(message);
 	}
 }
