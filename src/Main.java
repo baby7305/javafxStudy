1593eb6960e9646952ae6403cce6a6a739c980cd
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-17 17:03:14 +0800

    双向绑定

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 import javafx.application.Application;
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	Stage window;
-	Button button;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -19,23 +19,22 @@ public class Main extends Application {
 		window = primaryStage;
 		window.setTitle("thenewboston");
 
-		IntegerProperty x = new SimpleIntegerProperty(3);
-		IntegerProperty y = new SimpleIntegerProperty();
+		//Input and labels
+		TextField userInput = new TextField();
+		userInput.setMaxWidth(200);
+		Label firstLabel = new Label("Welcome to the site ");
+		Label secondLabel = new Label();
 
-		y.bind(x.multiply(10));
-		System.out.println("x: " + x.getValue());
-		System.out.println("y: " + y.getValue() + "\n");
+		HBox bottomText = new HBox(firstLabel, secondLabel);
+		bottomText.setAlignment(Pos.CENTER);
+		VBox vBox = new VBox(10, userInput, bottomText);
+		vBox.setAlignment(Pos.CENTER);
 
-		x.setValue(9);
-		System.out.println("x: " + x.getValue());
-		System.out.println("y: " + y.getValue() + "\n");
+		secondLabel.textProperty().bind(userInput.textProperty());
 
-		button = new Button("Submit");
-
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
-		Scene scene = new Scene(layout, 300, 250);
+		Scene scene = new Scene(vBox, 300, 200);
 		window.setScene(scene);
 		window.show();
 	}
+
 }
