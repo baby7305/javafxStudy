872839ff3d3edac4678b62bcc650ccd9eeb87176
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-17 15:39:19 +0800

    css测试

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	Button button;
+	Stage window;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -13,17 +16,49 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Title of the window");
+		window = primaryStage;
+		window.setTitle("thenewboston - JavaFX");
 
-		button = new Button();
-		button.setText("Click me");
+		//GridPane with 10px padding around edge
+		GridPane grid = new GridPane();
+		grid.setPadding(new Insets(10, 10, 10, 10));
+		grid.setVgap(8);
+		grid.setHgap(10);
 
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
+		//Name Label - constrains use (child, column, row)
+		Label nameLabel = new Label("Username:");
+		nameLabel.setId("bold-label");
+		GridPane.setConstraints(nameLabel, 0, 0);
 
-		Scene scene = new Scene(layout, 300, 250);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		//Name Input
+		TextField nameInput = new TextField("Bucky");
+		GridPane.setConstraints(nameInput, 1, 0);
 
+		//Password Label
+		Label passLabel = new Label("Password:");
+		GridPane.setConstraints(passLabel, 0, 1);
+
+		//Password Input
+		TextField passInput = new TextField();
+		passInput.setPromptText("password");
+		GridPane.setConstraints(passInput, 1, 1);
+
+		//Login
+		Button loginButton = new Button("Log In");
+		GridPane.setConstraints(loginButton, 1, 2);
+
+		//Sign up
+		Button signUpButton = new Button("Sign Up");
+		signUpButton.getStyleClass().add("button-blue");
+		GridPane.setConstraints(signUpButton, 1, 3);
+
+		//Add everything to grid
+		grid.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginButton, signUpButton);
+
+		Scene scene = new Scene(grid, 300, 200);
+		scene.getStylesheets().add("Viper.css");
+		window.setScene(scene);
+		window.show();
 	}
+
 }
