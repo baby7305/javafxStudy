b00c6b33ccc196616743962510a13bcd0cf49e57
宋海贝 <songhaibei@shmingjiang.com.cn>
2018-01-17 14:45:35 +0800

    menu测试

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	Button button;
+	Stage window;
+	BorderPane layout;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -13,17 +14,65 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		primaryStage.setTitle("Title of the window");
+		window = primaryStage;
+		window.setTitle("thenewboston");
 
-		button = new Button();
-		button.setText("Click me");
+		//File menu
+		Menu fileMenu = new Menu("File");
+		MenuItem newFile = new MenuItem("New...");
+		newFile.setOnAction(e -> System.out.println("Create a new file..."));
+		fileMenu.getItems().add(newFile);
+		fileMenu.getItems().add(new MenuItem("Open..."));
+		fileMenu.getItems().add(new MenuItem("Save..."));
+		fileMenu.getItems().add(new SeparatorMenuItem());
+		fileMenu.getItems().add(new MenuItem("Settings..."));
+		fileMenu.getItems().add(new SeparatorMenuItem());
+		fileMenu.getItems().add(new MenuItem("Exit..."));
 
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
+		//Edit menu
+		Menu editMenu = new Menu("_Edit");
+		editMenu.getItems().add(new MenuItem("Cut"));
+		editMenu.getItems().add(new MenuItem("Copy"));
+		MenuItem paste = new MenuItem("Paste");
+		paste.setOnAction(e -> System.out.println("Pasting some crap"));
+		paste.setDisable(true);
+		editMenu.getItems().add(paste);
 
-		Scene scene = new Scene(layout, 300, 250);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		//Help menu
+		Menu helpMenu = new Menu("Help");
+		CheckMenuItem showLines = new CheckMenuItem("Show Line Numbers");
+		showLines.setOnAction(e -> {
+			if(showLines.isSelected())
+				System.out.println("Program will now display line numbers");
+			else
+				System.out.println("Hiding line number");
+		});
+		CheckMenuItem autoSave = new CheckMenuItem("Enable Autosave");
+		autoSave.setSelected(true);
+		helpMenu.getItems().addAll(showLines, autoSave);
 
+		//Difficulty RadioMenuItems
+		Menu difficultyMenu = new Menu("Difficulty");
+		ToggleGroup difficultyToggle = new ToggleGroup();
+
+		RadioMenuItem easy = new RadioMenuItem("Easy");
+		RadioMenuItem medium = new RadioMenuItem("Medium");
+		RadioMenuItem hard = new RadioMenuItem("Hard");
+
+		easy.setToggleGroup(difficultyToggle);
+		medium.setToggleGroup(difficultyToggle);
+		hard.setToggleGroup(difficultyToggle);
+
+		difficultyMenu.getItems().addAll(easy, medium, hard);
+
+		//Main menu bar
+		MenuBar menuBar = new MenuBar();
+		menuBar.getMenus().addAll(fileMenu, editMenu, helpMenu, difficultyMenu);
+
+		layout = new BorderPane();
+		layout.setTop(menuBar);
+		Scene scene = new Scene(layout, 400, 300);
+		window.setScene(scene);
+		window.show();
 	}
 }
